f15931f20dec147e82e05c962a7ef57d7c052188
BeachBear <54738810+BeachBear@users.noreply.github.com>
2021-03-20 09:04:41 -0700

    Update betterdollhouse.lua

--- betterdollhouse.lua
+++ betterdollhouse.lua
@@ -1,39 +1,54 @@
 if game.PlaceId ~= 417267366 then
     return
-end if not game:GetService("Players").LocalPlayer then
+end
+if not game:GetService("Players").LocalPlayer then
     game:GetService("Players").PlayerAdded:wait()
-end if not game:IsLoaded() then
+end
+if not game:IsLoaded() then
     game.Loaded:wait()
 end
+if getgenv().BDH_BUFFERING then
+    return
+end
+
+getgenv().BDH_BUFFERING = true
 
 local loadedModel = game:GetObjects("rbxassetid://6520782286")[1]
 
+wait(0.06)
+
 if BetterDollhouse then
-    for _,v in pairs(getgenv().BetterDollhouse.Seats) do
-        v.Parent = loadedModel
+    getgenv().BetterDollhouse.Buffering = true
+    for _,save in ipairs(getgenv().BetterDollhouse.Save) do
+        save.Parent = loadedModel
     end
 else
     getgenv().BetterDollhouse = {
-        Seats = {};
+        Save = {};
+        Buffering = true;
     }
-
+    
     for _,v in ipairs(workspace:GetDescendants()) do
-        if v.ClassName == "Seat" then
-            getgenv().BetterDollhouse.Seats[v] = v
+        if v.ClassName == "Seat" or v:FindFirstChild("TouchInterest") then
+            v.Transparency = 1
             v.Parent = loadedModel
+            getgenv().BetterDollhouse.Save[#getgenv().BetterDollhouse.Save+1] = v
         end
     end
 end
 
-for _,v in ipairs(game:GetService("Lighting"):GetChildren()) do
-    v:Destroy()
-end
+wait(0.06)
 
+game:GetService("Lighting"):ClearAllChildren()
 for _,v in ipairs(workspace:GetChildren()) do
     if not v:FindFirstChild("Humanoid") and not (v.ClassName == "Camera" or v.ClassName == "Terrain") then
         v:Destroy()
     end
 end
 
-loadstring(loadedModel:GetAttribute("BootString"))()
 loadedModel.Parent = workspace
+loadstring(loadedModel:GetAttribute("BootString"))()
+
+wait()
+
+getgenv().BDH_BUFFERING = nil
